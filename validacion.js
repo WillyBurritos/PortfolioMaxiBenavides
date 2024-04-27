c7cfeae2629a03e0c9db558d31e0434d207d4360
Maximiliano Benavides <108031856+WillyBurritos@users.noreply.github.com>
2024-04-27 13:47:40 -0300

    Add files via upload

---
+++ validacion.js
@@ -0,0 +1,8 @@
+//Haz tú validación en javascript acá
+
+document.getElementById('back-to-top-btn').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
